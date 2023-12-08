8d1dfef0ca390ec7977f606e81c745b8fe68a6e4
Zuolin Wei <devys@foxmail.com>
2023-12-08 13:22:17 +0800

    Updated display

--- defective_surface_code_adapter/analyzer/data.py
+++ defective_surface_code_adapter/analyzer/data.py
@@ -1,20 +1,27 @@
 import dataclasses
 
+
 @dataclasses.dataclass
 class AnalysisResult:
     """A data class maintain the analysis result of a device."""
+
     x_distance: int
     x_shortest_paths_count: int
     z_distance: int
     z_shortest_paths_count: int
-    stabilizer_statistics: dict    
+    stabilizer_statistics: dict
 
     def __str__(self):
         # Pretty output.
-        return f"""\
+        output = (
+            [
+                f"""\
 x_distance: {self.x_distance}
 x_shortest_paths_count: {self.x_shortest_paths_count}
 z_distance: {self.z_distance}
-z_shortest_paths_count: {self.z_shortest_paths_count}
-stabilizer_statistics: {self.stabilizer_statistics}\
+z_shortest_paths_count: {self.z_shortest_paths_count}\
 """
+            ]
+            + [f"{key}: {value}" for key, value in self.stabilizer_statistics.items()]
+        )
+        return "\n".join(output)
