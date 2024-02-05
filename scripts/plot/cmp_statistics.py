fbaa1d1534e0fde73b87ba53c68571331d5346ac
Zuolin Wei <devys@foxmail.com>
2024-02-05 14:07:28 +0800

    Updated plot

--- scripts/plot/cmp_statistics.py
+++ scripts/plot/cmp_statistics.py
@@ -4,6 +4,17 @@ from typing import Dict, Any
 import numpy as np
 from matplotlib.lines import Line2D
 from matplotlib.patches import Patch
+import itertools
+
+plt.figure(figsize=(12, 9), dpi=100)
+
+marker = itertools.cycle(("o", "o", "^", "^", "s", "s"))
+color = itertools.cycle(
+    ("#ff7f0e", "#ff7f0e", "#1f77b4", "#1f77b4", "#2ca02c", "#2ca02c")
+)
+
+markersize = 4
+legend_fontsize = 10
 
 distances = [15, 21, 27]
 defect_rates = [0.005, 0.01, 0.015, 0.02]
@@ -12,78 +23,191 @@ defect_rates = [0.005, 0.01, 0.015, 0.02]
 with open("sp_data/cmp_statistics.json", "r") as f:
     results: Dict[str, Dict[str, Dict[str, Dict[str, Dict[str, Any]]]]] = json.load(f)
 
+plt.subplot(221)
 
-offset = {
-    0.005: -0.06,
-    0.01: -0.12,
-    0.015: -0.18,
-    0.02: -0.24,
-}
-
-box_width = 0.04
-
-color_box = {
-    0.02: '#ff9896',
-    0.015: '#ffbb78',
-    0.01: '#aec7e8',
-    0.005: '#98df8a',
-}
-
-color_box_dark = {
-    0.02: '#d62728',
-    0.015: '#d95f02',
-    0.01: '#1f77b4',
-    0.005: '#2ca02c',
-}
-
-# x_distance
-for dr in defect_rates:
-    plt.boxplot(
+for d in distances:
+    plt.plot(
+        defect_rates,
         [
-            [
-                result["Bandage"]["x_distance"]
-                for result in results[str(d)][str(dr)].values()
-            ]
-            for d in distances
+            np.average(
+                [
+                    result["Tradition"]["x_distance"]
+                    for result in results[str(d)][str(defect_rate)].values()
+                ]
+            )
+            for defect_rate in defect_rates
         ],
-        positions=[i + 1 + offset[dr] for i, d in enumerate(distances)],
-        widths=box_width,
-        labels=[15, 21, 27],
-        patch_artist=True,
-        boxprops=dict(facecolor=color_box[dr], edgecolor="black"),
-        medianprops=dict(color="black"),
-        flierprops=dict(marker=".", markerfacecolor=color_box[dr]),
+        label=f"T, L={d}",
+        marker=next(marker),
+        markersize=markersize,
+        color=next(color),
+        linestyle="--",
     )
 
-    plt.boxplot(
+    plt.plot(
+        defect_rates,
         [
-            [
-                result["Tradition"]["x_distance"]
-                for result in results[str(d)][str(dr)].values()
-            ]
-            for d in distances
+            np.average(
+                [
+                    result["Bandage"]["x_distance"]
+                    for result in results[str(d)][str(defect_rate)].values()
+                ]
+            )
+            for defect_rate in defect_rates
         ],
-        positions=[i + 1.3 + offset[dr] for i, d in enumerate(distances)],
-        widths=box_width,
-        labels=[15, 21, 27],
-        patch_artist=True,
-        boxprops=dict(facecolor=color_box_dark[dr], edgecolor="black"),
-        medianprops=dict(color="black"),
-        flierprops=dict(marker=".", markerfacecolor=color_box[dr]),
+        label=f"B, L={d}",
+        marker=next(marker),
+        markersize=markersize,
+        color=next(color),
     )
 
-plt.xticks(np.arange(1, 4), [15, 21, 27])
-
-legend_handles = [
-    Patch(facecolor=color_box[dr], edgecolor='black', label=f"B, DR={dr}") for dr in defect_rates
-] + [
-    Patch(facecolor=color_box_dark[dr], edgecolor='black', label=f"T, DR={dr}") for dr in defect_rates
-]
-
-plt.legend(handles=legend_handles, ncol=2, fontsize='8')
-
-plt.title("X Distance for Bandage and Tradition")
+plt.xticks(defect_rates)
+plt.xlabel("Defect rate")
+plt.ylabel("Average X Distance")
+plt.legend(fontsize=legend_fontsize)
 plt.grid()
-plt.xlabel("Code Size $L$")
-plt.ylabel("X Distance")
-plt.show()
+
+plt.subplot(222)
+
+for d in distances:
+    plt.plot(
+        defect_rates,
+        [
+            np.average(
+                [
+                    result["Tradition"]["z_distance"]
+                    for result in results[str(d)][str(defect_rate)].values()
+                ]
+            )
+            for defect_rate in defect_rates
+        ],
+        label=f"T, L={d}",
+        marker=next(marker),
+        markersize=markersize,
+        color=next(color),
+        linestyle="--",
+    )
+
+    plt.plot(
+        defect_rates,
+        [
+            np.average(
+                [
+                    result["Bandage"]["z_distance"]
+                    for result in results[str(d)][str(defect_rate)].values()
+                ]
+            )
+            for defect_rate in defect_rates
+        ],
+        label=f"B, L={d}",
+        marker=next(marker),
+        markersize=markersize,
+        color=next(color),
+    )
+
+plt.xticks(defect_rates)
+plt.xlabel("Defect rate")
+plt.ylabel("Average Z Distance")
+plt.legend(fontsize=legend_fontsize)
+plt.grid()
+
+plt.subplot(223)
+
+for d in distances:
+    plt.plot(
+        defect_rates,
+        [
+            np.average(
+                [
+                    result["Tradition"]["disabled_qubit_percentage"]
+                    for result in results[str(d)][str(defect_rate)].values()
+                ]
+            )
+            for defect_rate in defect_rates
+        ],
+        label=f"T, L={d}",
+        marker=next(marker),
+        markersize=markersize,
+        color=next(color),
+        linestyle="--",
+    )
+
+    plt.plot(
+        defect_rates,
+        [
+            np.average(
+                [
+                    result["Bandage"]["disabled_qubit_percentage"]
+                    for result in results[str(d)][str(defect_rate)].values()
+                ]
+            )
+            for defect_rate in defect_rates
+        ],
+        label=f"B, L={d}",
+        marker=next(marker),
+        markersize=markersize,
+        color=next(color),
+    )
+
+plt.xticks(defect_rates)
+plt.xlabel("Defect rate")
+plt.ylabel("Average Disabled Qubit Percentage")
+plt.legend(fontsize=legend_fontsize)
+plt.grid()
+
+plt.subplot(224)
+
+for d in distances:
+    plt.plot(
+        defect_rates,
+        [
+            np.average(
+                [
+                    result["Tradition"]["stabilizer_statistics"][
+                        "avg_stabilizer_weight"
+                    ]
+                    for result in results[str(d)][str(defect_rate)].values()
+                    if result["Bandage"]["stabilizer_statistics"][
+                        "avg_stabilizer_weight"
+                    ]
+                    is not None
+                ]
+            )
+            for defect_rate in defect_rates
+        ],
+        label=f"T, L={d}",
+        marker=next(marker),
+        markersize=markersize,
+        color=next(color),
+        linestyle="--",
+    )
+
+    plt.plot(
+        defect_rates,
+        [
+            np.average(
+                [
+                    result["Bandage"]["stabilizer_statistics"]["avg_stabilizer_weight"]
+                    for result in results[str(d)][str(defect_rate)].values()
+                    if result["Bandage"]["stabilizer_statistics"][
+                        "avg_stabilizer_weight"
+                    ]
+                    is not None
+                ]
+            )
+            for defect_rate in defect_rates
+        ],
+        label=f"B, L={d}",
+        marker=next(marker),
+        markersize=markersize,
+        color=next(color),
+    )
+
+plt.xticks(defect_rates)
+plt.xlabel("Defect rate")
+plt.ylabel("Average Super-Stabilizer Weight")
+plt.legend(fontsize=legend_fontsize)
+plt.grid()
+
+
+plt.savefig("cmp_statistics.pdf", format="pdf")
