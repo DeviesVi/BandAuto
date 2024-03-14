6fc70ab5ad48abb0c62941504333a43952b8bfa0
Zuolin Wei <devys@foxmail.com>
2024-03-14 19:22:05 +0800

    Updated script

--- scripts/sample/sample_over_specified_cycle.py
+++ scripts/sample/sample_over_specified_cycle.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import pickle
 from defective_surface_code_adapter import (
     SinterSampler, 
@@ -8,52 +7,54 @@ from defective_surface_code_adapter import (
     PhysicalErrors,
 )
 
-d = 15
-
-source_dir = f'device_pool/device_d{d}_qdr0.02_cdr0.02/devices'
-destination_dir = f'device_pool/device_d{d}_qdr0.02_cdr0.02/samples_over_specified_cycle'
-
-# Check destination directory exists
-if not os.path.exists(destination_dir):
-    os.makedirs(destination_dir)
-
 sampler = SinterSampler(num_workers=100)
-holding_cycle_option = HoldingCycleOption.SPECIFIED
 
-physical_errors = PhysicalErrors.SI1000_from_p(0.002)
-specified_cycle_range = range(1, (d+1)//2)
-
-def gen_tasks(device: Device):
+def gen_tasks(device: Device, shell_size_range: range, physical_errors: PhysicalErrors):
         print(f'Generating samples for device {device.strong_id}')
         distance = min(device.data_width, device.data_height)
-        print(f'holding_cycle_option: {holding_cycle_option.name}')
-        for specified_cycle in specified_cycle_range:
-            print(f'specified_cycle: {specified_cycle}')
+        for shell_size in shell_size_range:
+            print(f'specified_cycle: {shell_size}')
             yield from sampler.gen_sinter_tasks(
                 device=device,
                 cycles=[distance],
                 initial_states=["0","+"],
                 physical_errors_list=[physical_errors],
-                holding_cycle_option=holding_cycle_option,
-                specified_holding_cycle=specified_cycle,
+                holding_cycle_option=HoldingCycleOption.SPECIFIED,
+                specified_holding_cycle=shell_size,
             )
 
 def main():
-    for file in os.listdir(source_dir):
-        device = Device.load(f'{source_dir}/{file}')
-        samples_path = f'{destination_dir}/samples_{device.strong_id}.pkl'
-        if os.path.exists(samples_path):
-            print(f'Skipping device {device.strong_id} as samples already exist')
-            continue
-        try:
-            samples = sampler.sample(gen_tasks(device))
-        except:
-            print(f'Failed to generate samples for device {device.strong_id}')
-            # Log
-            with open('failed_devices.txt', 'a') as f:
-                f.write(f'{device.strong_id}\n')
-            continue
-        pickle.dump(samples, open(samples_path, 'wb'))
+    ds = [15, 21, 27]
+    defect_rates = [0.005, 0.01, 0.015, 0.02]
+
+    p = 0.002
+    physical_errors = PhysicalErrors.SI1000_from_p(p)
+    shell_size_range = range(1, (d+1)//2)
+
+    for d in ds:
+        for dr in defect_rates:
+            source_dir = f'device_pool/qubit_equal_coupler/device_d{d}_qdr{dr}_cdr{dr}/devices'
+            destination_dir = f'data/samples/global_shell/samples_d{d}_qdr{dr}_cdr{dr}_p{p}'
+
+            # Check destination directory exists
+            if not os.path.exists(destination_dir):
+                os.makedirs(destination_dir)
+
+            for file in os.listdir(source_dir):
+                device = Device.load(f'{source_dir}/{file}')
+                samples_path = f'{destination_dir}/samples_{device.strong_id}.pkl'
+                if os.path.exists(samples_path):
+                    print(f'Skipping device {device.strong_id} as samples already exist')
+                    continue
+                try:
+                    samples = sampler.sample(gen_tasks(device, shell_size_range, physical_errors))
+                except:
+                    print(f'Failed to generate samples for device {device.strong_id}')
+                    # Log
+                    with open('failed_devices.txt', 'a') as f:
+                        f.write(f'{device.strong_id}\n')
+                    continue
+                pickle.dump(samples, open(samples_path, 'wb'))
 
 
 # NOTE: This is actually necessary! If the code inside 'main()' was at the
