1c7a493f9bd7e414f16e7ba5f063fe3dd9db8476
Zuolin Wei <devys@foxmail.com>
2024-04-22 17:15:17 +0800

    Added a new script for integrity

---
+++ scripts/sample/gen_device_pool_perfect.py
@@ -0,0 +1,28 @@
+from defective_surface_code_adapter import Device
+import os
+
+ds = [15, 21, 27]
+drs = [0]
+
+for d in ds:
+    for dr in drs:
+        destination_dir = f'device_pool/perfect/device_d{d}_qdr{dr}_cdr{dr}/devices'
+
+        # Create destination directory if it does not exist
+        if not os.path.exists(destination_dir):
+            os.makedirs(destination_dir)
+
+
+        # Parameters for generating devices
+        width = d
+        height = d
+        qubit_defect_rate = dr
+        coupler_defect_rate = dr
+
+        device_count = 1
+
+        # Generate devices
+        device = Device(width, height)
+        for i in range(device_count):
+            device.add_random_defect(qubit_defect_rate, coupler_defect_rate)
+            device.save(f'{destination_dir}/device_{device.strong_id}.pkl')
